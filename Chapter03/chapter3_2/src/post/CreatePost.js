37a0d9971a723490277e608bcf0e866d5ff6973d
Daniel Bugl <me@omnidan.net>
2019-04-25 12:30:52 +0200

    use <div> instead of <form>

--- Chapter03/chapter3_2/src/post/CreatePost.js
+++ Chapter03/chapter3_2/src/post/CreatePost.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function CreatePost ({ user }) {
   return (
-    <form>
+    <div>
       <div>Author: <b>{user}</b></div>
       <div>Title: <input type="text" /></div>
       <textarea />
-    </form>
+    </div>
   )
 }
