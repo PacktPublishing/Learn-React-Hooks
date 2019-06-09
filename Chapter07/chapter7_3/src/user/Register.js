c7a3d7b0de2af4dbe781efa42b4760cd69ce89a8
Daniel Bugl <me@omnidan.net>
2019-06-09 17:31:19 +0200

    finish chapter7_2

---
+++ Chapter07/chapter7_3/src/user/Register.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { useResource } from 'react-request-hook'
+import { StateContext } from '../contexts'
+
+export default function Register () {
+    const { dispatch } = useContext(StateContext)
+    
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ passwordRepeat, setPasswordRepeat ] = useState('')
+
+    const [ user, register ] = useResource((username, password) => ({
+        url: '/users',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: 'REGISTER', username: user.data.username })
+        }
+    }, [user])
+
+    function handleUsername (evt) {
+        setUsername(evt.target.value)
+    }
+
+    function handlePassword (evt) {
+      setPassword(evt.target.value)
+    }
+    
+    function handlePasswordRepeat (evt) {
+        setPasswordRepeat(evt.target.value)
+    }
+
+    return (
+        <div>
+            Username: <input type="text" value={username} onChange={handleUsername} />
+            Password: <input type="password" value={password} onChange={handlePassword} />
+            Repeat password: <input type="password" value={passwordRepeat} onChange={handlePasswordRepeat} />
+            <input type="submit" value="Register" onClick={() => register(username, password)} disabled={username.length === 0 || password.length === 0 || password !== passwordRepeat} />
+        </div>
+    )
+}
