4d1948a161142d520c8508fe9e493bdf34724590
Daniel Bugl <me@omnidan.net>
2019-06-17 12:51:41 +0200

    implement responsive hiding

--- Chapter08/chapter8_2/src/pages/HeaderBar.js
+++ Chapter08/chapter8_2/src/pages/HeaderBar.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import useWindowSize from '@rehooks/window-size'
 
 import CreatePost from '../post/CreatePost'
 import UserBar from '../user/UserBar'
@@ -12,15 +13,18 @@ export default function HeaderBar ({ setTheme }) {
     const { state } = useContext(StateContext)
     const { user } = state
 
+    const { innerWidth } = useWindowSize()
+    const mobilePhone = innerWidth < 640
+
     return (
         <div>
             <Header text="React Hooks Blog" />
-            <ChangeTheme theme={theme} setTheme={setTheme} />
-            <br />
-            <React.Suspense fallback={"Loading..."}>
+            {!mobilePhone && <ChangeTheme theme={theme} setTheme={setTheme} />}
+            {!mobilePhone && <br />}
+            {!mobilePhone && <React.Suspense fallback={"Loading..."}>
                 <UserBar />
-            </React.Suspense>
-            <br />
+            </React.Suspense>}
+            {!mobilePhone && <br />}
             {user && <CreatePost />}
         </div>
     )
