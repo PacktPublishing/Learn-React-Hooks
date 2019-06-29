22614239d3a4ae1a1cd45e6bdd259c73474df122
Daniel Bugl <me@omnidan.net>
2019-06-29 10:38:16 +0200

    init chapter10_1

---
+++ Chapter10/chapter10_1/src/post/CreatePost.js
@@ -0,0 +1,69 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { useInput } from 'react-hookedup-fork'
+import useUndo from 'use-undo'
+import { useDebouncedCallback } from 'use-debounce'
+import { useNavigation } from 'react-navi'
+import { useResource } from 'react-request-hook'
+import { StateContext } from '../contexts'
+
+export default function CreatePost () {
+  const { state, dispatch } = useContext(StateContext)
+  const { user } = state
+
+  const { value: title, bindToInput: bindTitle } = useInput('')
+  const [ content, setInput ] = useState('')
+  const [ undoContent, {
+    set: setContent,
+    undo,
+    redo,
+    canUndo,
+    canRedo
+  } ] = useUndo('')
+
+  const [ setDebounce, cancelDebounce ] = useDebouncedCallback(
+    (value) => {
+      setContent(value)
+    },
+    200
+  )
+  useEffect(() => {
+    cancelDebounce()
+    setInput(undoContent.present)
+  }, [cancelDebounce, undoContent])
+
+  const [ post, createPost ] = useResource(({ title, content, author }) => ({
+    url: '/posts',
+    method: 'post',
+    data: { title, content, author }
+  }))
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    if (post && post.data) {
+      navigation.navigate(`/view/${post.data.id}`)
+    }
+  }, [navigation, post])
+
+  function handleContent (e) {
+    const { value } = e.target
+    setInput(value)
+    setDebounce(value)
+  }
+
+  function handleCreate () {
+    createPost({ title, content, author: user })
+    dispatch({ type: 'CREATE_POST', title, content, author: user })
+  }
+
+  return (
+    <div>
+      <div>Author: <b>{user}</b></div>
+      <div>Title: <input type="text" value={title} {...bindTitle} /></div>
+      <textarea value={content} onChange={handleContent} />
+      <button onClick={undo} disabled={!canUndo}>Undo</button>
+      <button onClick={redo} disabled={!canRedo}>Redo</button>
+      <input type="submit" value="Create" onClick={handleCreate} />
+    </div>
+  )
+}
---
+++ Chapter10/chapter10_1/src/user/Login.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { useInput } from 'react-hookedup-fork'
+import { useResource } from 'react-request-hook'
+import { StateContext } from '../contexts'
+
+export default function Login () {
+  const { dispatch } = useContext(StateContext)
+  
+  const { value: username, bindToInput: bindUsername } = useInput('')
+  const [ loginFailed, setLoginFailed ] = useState(false)
+  const { value: password, bindToInput: bindPassword } = useInput('')
+
+  const [ user, login ] = useResource((username, password) => ({
+    url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+    method: 'get'
+  }))
+
+  useEffect(() => {
+    if (user && user.data) {
+      if ( user.data.length > 0) {
+        setLoginFailed(false)
+        dispatch({ type: 'LOGIN', username: user.data[0].username })
+      } else {
+        setLoginFailed(true)
+      }
+    }
+    if (user && user.error) {
+      setLoginFailed(true)
+    }
+  }, [dispatch, user])
+
+  return (
+    <div>
+      Username: <input type="text" value={username} {...bindUsername} />
+      Password: <input type="password" value={password} {...bindPassword} />
+      <input type="submit" value="Login" onClick={() => login(username, password)} disabled={username.length === 0} />
+      {loginFailed && <span style={{ color: 'red' }}>Invalid username or password</span>}
+    </div>
+  )
+}
