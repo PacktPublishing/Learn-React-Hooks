2cf104978687fa37839387d356655b296812308f
Daniel Bugl <me@omnidan.net>
2019-09-11 15:05:59 +0200

    pass empty user to setUser

--- Chapter03/chapter3_3/src/user/Logout.js
+++ Chapter03/chapter3_3/src/user/Logout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Logout ({ user, setUser }) {
   return (
-    <form onSubmit={e => { e.preventDefault(); setUser(false) }}>
+    <form onSubmit={e => { e.preventDefault(); setUser('') }}>
       Logged in as: <b>{user}</b>
       <input type="submit" value="Logout" />
     </form>
