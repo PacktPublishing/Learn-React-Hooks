1c299f314acdf5150f9915a56e112ab022f4564a
Daniel Bugl <me@omnidan.net>
2019-09-12 10:02:04 +0200

    add nested providers example

--- Chapter05/chapter5_1/src/App.js
+++ Chapter05/chapter5_1/src/App.js
@@ -6,7 +6,10 @@ export const ThemeContext = React.createContext({ primaryColor: 'deepskyblue' })
 
 const App = () => (
   <ThemeContext.Provider value={{ primaryColor: 'coral' }}>
-    <Header text="Hello World" />
+      <Header text="Hello World" />
+      <ThemeContext.Provider value={{ primaryColor: 'deepskyblue' }}>
+          <Header text="This is a test" />
+      </ThemeContext.Provider>
   </ThemeContext.Provider>
 )
 
