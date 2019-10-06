9a175d2c97b3ed9606012eaf75dc9e6fa8fc0d08
Daniel Bugl <me@omnidan.net>
2019-10-06 13:47:55 +0200

    fix typo in name

--- Chapter08/chapter8_3/src/examples/UseOnlineStatus.js
+++ Chapter08/chapter8_3/src/examples/UseOnlineStatus.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useOnlineStatus } from 'react-hookedup'
 
-export default function UseOnLineStatus () {
+export default function UseOnlineStatus () {
     const { online } = useOnlineStatus()
 
     return <div>You are {online ? 'online' : 'offline'}!</div>
