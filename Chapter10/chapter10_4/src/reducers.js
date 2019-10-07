c5b6f4e5b58ca4eb54b3bbdf606ef4534bf64648
Daniel Bugl <me@omnidan.net>
2019-10-07 19:47:49 +0200

    chapter10_4: fix post id being passed to action

--- Chapter10/chapter10_4/src/reducers.js
+++ Chapter10/chapter10_4/src/reducers.js
@@ -18,7 +18,7 @@ function postsReducer (state, action) {
           return action.posts
 
       case 'CREATE_POST':
-          const newPost = { title: action.title, content: action.content, author: action.author }
+          const newPost = { title: action.title, content: action.content, author: action.author, id: action.id }
           return [ newPost, ...state ]
       
       default:
